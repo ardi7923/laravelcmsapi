0de5ee5abbefb52687c0ad0bca0db03532936e42
ardi7923 <ardiirawan777@gmail.com>
2024-05-18 23:47:49 +0800

    laravelcmsprovider

---
+++ src/LaravelcmsServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ardi7923\Laravelcmsapi;
+
+
+class LaravelcmsapiServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Boot the instance, add macros for datatable engines.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+//        $this->publishes([
+//            __DIR__.'/resources/lang' => resource_path('lang/en'),
+//        ]);
+//
+//        $this->publishes([
+//            __DIR__.'/assets/js/share' => public_path('js'),
+//        ]);
+
+//        if ($this->app->runningInConsole()) {
+//            $this->commands([
+//                CrudAjax::class,
+//                CrudAjaxBladeCompiler::class,
+//                CrudAjaxSetCommand::class
+//            ]);
+//        }
+    }
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
